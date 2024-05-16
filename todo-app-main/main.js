234497c02668915b2f4e9d862952c52b635c2a36
sonnie36 <sonniemwati36@gmail.com>
2024-05-16 06:26:36 -0700

    Added delete function and a function that checks the icon if task is completed and puts a line through

--- todo-app-main/main.js
+++ todo-app-main/main.js
@@ -23,28 +23,63 @@ function AddCardTask(task) {
     
     const TaskField = document.createElement('div');
     TaskField.classList.add('taskField');
-     
-    const checkIcon = document.createElement('img');
-    checkIcon.src = 'images/icon-check.svg';
-    checkIcon.classList.add('check-icon');
+   
+    const checkDiv = document.createElement('div')
+    checkDiv.classList.add('checkSection');
+
+    const checkIcon = document.createElement('input')
+    checkIcon.setAttribute('type', 'checkbox')
+    checkIcon.classList.add('circle')
+    checkIcon.checked = task.Completed;
+    checkIcon.addEventListener('change', function() {
+        task.Completed = this.checked;
+        taskDesc.style.textDecoration = this.checked ? "line-through" : "none";
+        localStorage.setItem('Tasks', JSON.stringify(allTasks));
+    });
 
     const taskDesc = document.createElement('p');
     taskDesc.textContent = task.Task;
-    if (task.Completed) {
-        taskDesc.style.textDecoration = "line-through";
-    }
+    taskDesc.style.textDecoration = task.Completed ? "line-through" : "none";
 
     const deleteIcon = document.createElement('img');
     deleteIcon.src = 'images/icon-cross.svg';
     deleteIcon.classList.add('delete-icon');
+    deleteIcon.addEventListener('click', function() {
+        const index = allTasks.indexOf(task);
+        allTasks.splice(index, 1);
+        localStorage.setItem('Tasks', JSON.stringify(allTasks));
+        card.removeChild(TaskField);
+    });
 
-    TaskField.appendChild(checkIcon);
-    TaskField.appendChild(taskDesc);
+    checkDiv.appendChild(checkIcon)
+    checkDiv.appendChild(taskDesc)
+
+    TaskField.appendChild(checkDiv);
     TaskField.appendChild(deleteIcon);
 
     card.appendChild(TaskField);
 }
 
+const refreshCard = ()=>{
+    const card = document.querySelector('.card');
+    while (card.firstChild) {
+        card.removeChild(card.firstChild);
+    }
+    for (let task of allTasks) {
+        AddCardTask(task);
+    }
+    updateItemsLeftCount();
+    }
+    
+const ALL = document.querySelector('.btnAll')
+ALL.addEventListener('click',()=>{
+    let tasks = JSON.parse(localStorage.getItem('Tasks'));
+    if(tasks){
+        allTasks = tasks;
+        refreshCard();
+    }
+
+})
 // Load tasks from localStorage on page load
 window.onload = function() {
     let tasks = JSON.parse(localStorage.getItem('Tasks'));
